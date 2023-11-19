1bfe067c841dd5bc5ab7a96ec14c851ea36f4e7e
muhammad45401 <saadjani3333@gmail.com>
2023-11-19 18:13:16 +0500

    arry-loop

---
+++ app.js
@@ -0,0 +1,64 @@
+// for( var i=0; i<=3;i++){
+//   for( var j=1; j<=i; j++){
+//     document.write(j)
+//     document.write(i)
+//     document.write(" ")
+//   }
+
+//   document.write(" <br>")
+// }
+
+for ( var i=0; i<=3; i++){
+    document.write(+i+ " ")
+    }
+
+    document.write( "<br>")
+    document.write( "<br>")
+
+
+    for ( var i=1; i<=10; i++){
+        document.write(i)
+        document.write( "<br>")
+    }
+
+    document.write( "<br>")
+    document.write( "<br>")
+
+
+var tableNum = prompt(" enter your table num")
+var startNum = prompt (" enter your start table number")
+var endingtNum = prompt (" enter your ending table number")
+
+
+for ( var i=startNum; i<=endingtNum; i++){
+    
+    document.write( tableNum + "X" + i + "=" + tableNum *i  )
+    
+    document.write( "<br>")
+}
+
+
+document.write( "<br>")
+document.write( "<br>")
+
+
+
+var fruits = ["apple" ,    "banana",    "mango",    "orange",  "strawberry" ]
+document.write(  fruits  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
