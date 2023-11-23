eb33d15030e87f789d96447adf17148e5cbb8176
muhammad45401 <saadjani3333@gmail.com>
2023-11-24 00:51:37 +0500

    arry-loop

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-// for( var i=0; i<=3;i++){
-//   for( var j=1; j<=i; j++){
-//     document.write(j)
-//     document.write(i)
-//     document.write(" ")
-//   }
 
-//   document.write(" <br>")
-// }
 
-for ( var i=0; i<=3; i++){
-    document.write(+i+ " ")
-    }
 
-    document.write( "<br>")
-    document.write( "<br>")
+// document.write("Q:1" + "<br> <br>")
+
+// var ary1 =[[1,2,3,4] ,[1,0,1,2],[2,1,0,1]];
+
+//    for (var i=0; i<=ary1.length;i++){
+//     for ( var j=0; j<=ary1.length;j++){
+//         document.write(ary1[i][j]+" "); 
+//     }
+//     document.write("<br>")
+//    };
+
+// problum====> agr uper wala arry uncomment krta hon nechy ka kam show ni 
+// hota agr comment krta hon phir nechy ka kam show krta hy
+  
 
 
     for ( var i=1; i<=10; i++){
@@ -25,27 +26,156 @@ for ( var i=0; i<=3; i++){
     document.write( "<br>")
 
 
-var tableNum = prompt(" enter your table num")
-var startNum = prompt (" enter your start table number")
-var endingtNum = prompt (" enter your ending table number")
+// var tableNum = prompt(" enter your table num")
+// var startNum = prompt (" enter your start table number")
+// var endingtNum = prompt (" enter your ending table number")
 
 
-for ( var i=startNum; i<=endingtNum; i++){
+// for ( var i=startNum; i<=endingtNum; i++){
     
-    document.write( tableNum + "X" + i + "=" + tableNum *i  )
+//     document.write( tableNum + "X" + i + "=" + tableNum *i  )
     
-    document.write( "<br>")
+//     document.write( "<br>")
+// }
+
+
+document.write( "<br>")
+document.write( "<br>")
+
+
+document.write("<b>")
+var fruits = ["apple" ,  "banana",    "mango",    "orange",  "strawberry" ]
+for ( var i=0; i<fruits.length; i++){
+    document.write(fruits[i] +" <br>")
+   
+}
+document.write("</b>")
+document.write("<br>")
+document.write("index num of"+" " +  fruits[0]+ " " + fruits.indexOf("apple"));
+document.write( "<br> <br>")
+document.write("index num of"+" " +  fruits[1]+ " " + fruits.indexOf("banana"));
+document.write( "<br> <br>")
+document.write("index num of"+" " +  fruits[2]+ " " + fruits.indexOf("mango"));
+document.write( "<br> <br>")
+document.write("index num of"+" " +  fruits[3]+ " " + fruits.indexOf("orange"));
+document.write( "<br> <br>")
+document.write("index num of"+" " +  fruits[4]+ " " + fruits.indexOf("strawberry"));
+
+
+
+
+
+document.write( "<br> <br> <br> <br>")
+
+
+
+
+for ( var i=1; i<=15; i++){
+    document.write(i +";"+ " ")
+}
+
+document.write( "<br> <br>")
+var con = "<h3> counting number</h3>"
+document.write(con)
+
+
+var cont =[1,2,3,4,5,6,7,8,9,10]
+document.write(cont)
+document.write( "<br>")
+var con = "<h3> counting reverse</h3>"
+document.write(con)
+cont.reverse()
+document.write(cont)
+
+
+document.write( "<br> <br>")
+
+var con = "<h3> Even number</h3>"
+document.write(con)
+
+
+
+for ( var i=0; i<=20; i++){
+    if( i%2 ==0){
+        document.write(i + " ")
+    }
+    
+}
+document.write( "<br> <br>")
+
+
+var con = "<h3> odd number</h3>"
+document.write(con)
+
+
+
+for ( var i=1; i<=20; i++){
+    if( i%2 !==0){
+        document.write(i + " ")
+    }
+    
 }
 
 
-document.write( "<br>")
-document.write( "<br>")
+document.write( "<br> <br>")
+
+var con = "<h3> Series number</h3>"
+document.write(con)
 
 
 
-var fruits = ["apple" ,    "banana",    "mango",    "orange",  "strawberry" ]
-document.write(  fruits  )
+for ( var i=1; i<=20; i++){
+    if( i%2 ==0){
+        document.write(i + " "+ "K; ")
+    }
+    
+}
 
+document.write( "<br> <br>")
+
+
+
+// var items =["cake",  "apple pie",  "cookie",  "chips",  "patties"]
+// var userItems = prompt (" Well Come I.K Bakery \n Enter your item ");
+
+// for( var i=0; i<=items.length; i++){
+// if(userItems==items[i]){
+
+// alert("your oder will be ready in 15 minutes"); 
+// document.write(userItems +" is available at index" +" " + items.indexOf(userItems) +" in our bakery .")
+// }else{
+//     alert("Sorry ! your item is not found")
+// }   document.write("we are Sorry !"  +userItems   +" is <b>not available </b> in our bakery .")
+// break
+// }
+
+document.write("<br> <br>")
+
+
+document.write("Q:10" + "<br> <br>")
+
+
+for ( var a=5; a<=100; a=a+5){
+ document.write(a + " ")
+}
+
+document.write("<br> <br>")
+
+
+
+
+
+
+document.write("Q:1" + "<br> <br>")
+
+var ary1 =[[1,2,3,4] ,[1,0,1,2],[2,1,0,1]];
+
+   for (var i=0; i<=ary1.length;i++){
+    for ( var j=0; j<=ary1.length;j++){
+        document.write(ary1[i][j]+" "); 
+    }
+    document.write("<br>")
+   }
 
 
 
